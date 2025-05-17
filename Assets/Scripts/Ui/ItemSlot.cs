5f91b9cee6b3c8bb9a44e3922dc89460e9d369c6
Voidintheheart <169116369+Voidintheheart@users.noreply.github.com>
2025-05-17 01:11:37 -0600

    a
    a

---
+++ Assets/Scripts/Ui/ItemSlot.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
+{
+    //=========ItemData==========//
+    public string itemName;
+    public int quantity;
+    public Sprite itemSprite;
+    public bool isFull;
+
+
+
+
+
+
+
+
+
+    //========ItemSlot===========//
+    [SerializeField]
+    private TMP_Text quantityText;
+
+    [SerializeField]
+    private Image itemImage;
+
+
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+
+    private InventoryManager inventoryManager;
+
+    public void Start()
+    {
+        inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.itemSprite = itemSprite;
+        isFull = true;
+
+        quantityText.text = quantity.ToString();
+        quantityText.enabled = true;
+        itemImage.sprite = itemSprite;
+    }
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLeftClick();
+        }
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+    }
+
+    public void OnLeftClick()
+    {
+        inventoryManager.DeselectAllSlots();
+        selectedShader.SetActive(true);
+        thisItemSelected = true; 
+    }
+
+    public void OnRightClick()
+    {
+
+    }
+
+}
